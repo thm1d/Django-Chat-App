acd4d09a8b9bd3292c6916e27ee147a676329088
Tahmid Rahman <tahmid11q@gmail.com>
2021-11-29 20:32:55 +0600

    Project Setup

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'ChatApp',
+    'user_control',
 ]
 
 MIDDLEWARE = [
